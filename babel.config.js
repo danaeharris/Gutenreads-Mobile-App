4e8f74e5bbcdcb9cf60268804db53904fe08c515
Danae Harris <hello@danaeharris.com>
2023-03-31 14:59:18 -0400

    continue adding mobx

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    "plugins": [["@babel/plugin-proposal-class-properties"]],
-    "assumptions": {
-        "setPublicClassFields": false
+    plugins: [["@babel/plugin-proposal-class-properties"]],
+    assumptions: {
+        setPublicClassFields: false
     }
   };
 };
